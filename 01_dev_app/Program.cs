269f64ef37115d60397f1d906860bea97777ff4b
szymongr <gramza.szymon@gmail.com>
2015-07-22 08:41:16 -0700

    initial commit

---
+++ 01_dev_app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_dev_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sorry, this application do nothing :)"); //no version
+            Console.ReadKey();
+        }
+    }
+}
